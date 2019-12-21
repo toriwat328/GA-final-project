c8c389865e47cc60094826c68d19ca9d7c4aa209
toriwat328 <toriwat328@gmail.com>
2019-12-21 17:16:49 -0500

    added functional map, map line background and title

---
+++ app.js
@@ -0,0 +1,324 @@
+
+
+$('#East_Harlem').on('click', () => {
+
+    if($('#East_Harlem').hasClass("black")){
+
+        $('#East_Harlem').removeClass("black");
+        $('#East_Harlem').addClass("red");
+
+
+    } else if($('#East_Harlem').hasClass("red")){
+        $('#East_Harlem').removeClass("red");
+        $('#East_Harlem').addClass("black");
+
+
+    }
+
+})
+
+$('#Morningside_Heights').on('click', () => {
+
+    if($('#Morningside_Heights').hasClass("black")){
+
+        $('#Morningside_Heights').removeClass("black");
+        $('#Morningside_Heights').addClass("red");
+
+
+    } else if($('#Morningside_Heights').hasClass("red")){
+        $('#Morningside_Heights').removeClass("red");
+        $('#Morningside_Heights').addClass("black");
+
+
+    }
+
+})
+
+$('#Upper_West_Side').on('click', () => {
+
+    if($('#Upper_West_Side').hasClass("black")){
+
+        $('#Upper_West_Side').removeClass("black");
+        $('#Upper_West_Side').addClass("red");
+
+
+    } else if($('#Upper_West_Side').hasClass("red")){
+        $('#Upper_West_Side').removeClass("red");
+        $('#Upper_West_Side').addClass("black");
+
+
+    }
+
+})
+
+$('#Central_Park').on('click', () => {
+
+    if($('#Central_Park').hasClass("black")){
+
+        $('#Central_Park').removeClass("black");
+        $('#Central_Park').addClass("red");
+
+
+    } else if($('#Central_Park').hasClass("red")){
+        $('#Central_Park').removeClass("red");
+        $('#Central_Park').addClass("black");
+
+
+    }
+
+})
+
+$('#Harlem').on('click', () => {
+
+    if($('#Harlem').hasClass("black")){
+
+        $('#Harlem').removeClass("black");
+        $('#Harlem').addClass("red");
+
+
+    } else if($('#Harlem').hasClass("red")){
+        $('#Harlem').removeClass("red");
+        $('#Harlem').addClass("black");
+
+
+    }
+
+})
+
+$('#Upper_East_Side').on('click', () => {
+
+    if($('#Upper_East_Side').hasClass("black")){
+
+        $('#Upper_East_Side').removeClass("black");
+        $('#Upper_East_Side').addClass("red");
+
+
+    } else if($('#Upper_East_Side').hasClass("red")){
+        $('#Upper_East_Side').removeClass("red");
+        $('#Upper_East_Side').addClass("black");
+
+
+    }
+
+})
+
+$('#Lenox_Hill').on('click', () => {
+
+    if($('#Lenox_Hill').hasClass("black")){
+
+        $('#Lenox_Hill').removeClass("black");
+        $('#Lenox_Hill').addClass("red");
+
+
+    } else if($('#Lenox_Hill').hasClass("red")){
+        $('#Lenox_Hill').removeClass("red");
+        $('#Lenox_Hill').addClass("black");
+
+
+    }
+
+})
+
+$('#Hell_s_Kitchen').on('click', () => {
+
+    if($('#Hell_s_Kitchen').hasClass("black")){
+
+        $('#Hell_s_Kitchen').removeClass("black");
+        $('#Hell_s_Kitchen').addClass("red");
+
+
+    } else if($('#Hell_s_Kitchen').hasClass("red")){
+        $('#Hell_s_Kitchen').removeClass("red");
+        $('#Hell_s_Kitchen').addClass("black");
+
+
+    }
+
+})
+
+$('#Theater_District').on('click', () => {
+
+    if($('#Theater_District').hasClass("black")){
+
+        $('#Theater_District').removeClass("black");
+        $('#Theater_District').addClass("red");
+
+
+    } else if($('#Theater_District').hasClass("red")){
+        $('#Theater_District').removeClass("red");
+        $('#Theater_District').addClass("black");
+
+
+    }
+
+})
+
+$('#Midtown').on('click', () => {
+
+    if($('#Midtown').hasClass("black")){
+
+        $('#Midtown').removeClass("black");
+        $('#Midtown').addClass("red");
+
+
+    } else if($('#Midtown').hasClass("red")){
+        $('#Midtown').removeClass("red");
+        $('#Midtown').addClass("black");
+
+
+    }
+
+})
+
+$('#Chelsea').on('click', () => {
+
+    if($('#Chelsea').hasClass("black")){
+
+        $('#Chelsea').removeClass("black");
+        $('#Chelsea').addClass("red");
+
+
+    } else if($('#Chelsea').hasClass("red")){
+        $('#Chelsea').removeClass("red");
+        $('#Chelsea').addClass("black");
+
+
+    }
+
+})
+
+$('#Nomad').on('click', () => {
+
+    if($('#Nomad').hasClass("black")){
+
+        $('#Nomad').removeClass("black");
+        $('#Nomad').addClass("red");
+
+
+    } else if($('#Nomad').hasClass("red")){
+        $('#Nomad').removeClass("red");
+        $('#Nomad').addClass("black");
+
+
+    }
+
+})
+
+$('#Kips_Bay').on('click', () => {
+
+    if($('#Kips_Bay').hasClass("black")){
+
+        $('#Kips_Bay').removeClass("black");
+        $('#Kips_Bay').addClass("red");
+
+
+    } else if($('#Kips_Bay').hasClass("red")){
+        $('#Kips_Bay').removeClass("red");
+        $('#Kips_Bay').addClass("black");
+
+
+    }
+
+})
+
+$('#West_Village').on('click', () => {
+
+    if($('#West_Village').hasClass("black")){
+
+        $('#West_Village').removeClass("black");
+        $('#West_Village').addClass("red");
+
+
+    } else if($('#West_Village').hasClass("red")){
+        $('#West_Village').removeClass("red");
+        $('#West_Village').addClass("black");
+
+
+    }
+
+})
+
+$('#Greenwich_Village').on('click', () => {
+
+    if($('#Greenwich_Village').hasClass("black")){
+
+        $('#Greenwich_Village').removeClass("black");
+        $('#Greenwich_Village').addClass("red");
+
+
+    } else if($('#Greenwich_Village').hasClass("red")){
+        $('#Greenwich_Village').removeClass("red");
+        $('#Greenwich_Village').addClass("black");
+
+
+    }
+
+})
+
+$('#East_Village').on('click', () => {
+
+    if($('#East_Village').hasClass("black")){
+
+        $('#East_Village').removeClass("black");
+        $('#East_Village').addClass("red");
+
+
+    } else if($('#East_Village').hasClass("red")){
+        $('#East_Village').removeClass("red");
+        $('#East_Village').addClass("black");
+
+
+    }
+
+})
+
+$('#Soho_ChinaTown_Tribeca').on('click', () => {
+
+    if($('#Soho_ChinaTown_Tribeca').hasClass("black")){
+
+        $('#Soho_ChinaTown_Tribeca').removeClass("black");
+        $('#Soho_ChinaTown_Tribeca').addClass("red");
+
+
+    } else if($('#Soho_ChinaTown_Tribeca').hasClass("red")){
+        $('#Soho_ChinaTown_Tribeca').removeClass("red");
+        $('#Soho_ChinaTown_Tribeca').addClass("black");
+
+
+    }
+
+})
+
+$('#Lower_East_Side').on('click', () => {
+
+    if($('#Lower_East_Side').hasClass("black")){
+
+        $('#Lower_East_Side').removeClass("black");
+        $('#Lower_East_Side').addClass("red");
+
+
+    } else if($('#Lower_East_Side').hasClass("red")){
+        $('#Lower_East_Side').removeClass("red");
+        $('#Lower_East_Side').addClass("black");
+
+
+    }
+
+})
+
+$('#Financial_District').on('click', () => {
+
+    if($('#Financial_District').hasClass("black")){
+
+        $('#Financial_District').removeClass("black");
+        $('#Financial_District').addClass("red");
+
+
+    } else if($('#Financial_District').hasClass("red")){
+        $('#Financial_District').removeClass("red");
+        $('#Financial_District').addClass("black");
+
+
+    }
+
+})
